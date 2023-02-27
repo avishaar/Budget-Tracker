c6e784b8903bd7a8e7687b2d33f517ffc338bb7c
avishaar <ashaar928@gmail.com>
2023-02-27 15:03:11 +0200

    to add new expense with error

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { v4 as uuidv4 } from 'uuid';
 
 const AddExpenseForm = () => {
+
+    const { dispatch } = useContext{AppContext};
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
 
     const onSubmit = (event) => {
+        event.preventDefault();
 
+        const expense = {
+            id: uuidv4(),
+            name: name,
+            cost: parseInt(cost),
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense,
+        });
     };
     return(
         <form onSubmit={onSubmit}>
