587c9fe1b2923e2e75f62551a8480cfb5bea0f1b
avishaar <ashaar928@gmail.com>
2023-02-28 12:50:05 +0200

    fixed eror

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -5,7 +5,7 @@ const ExpenseTotal = () => {
 
 	const { expenses } = useContext(AppContext);
 
-	const totalExpenses = expense.reduce((total, item) => {
+	const totalExpenses = expenses.reduce((total, item) => {
 		return (total += item.cost);
 	}, 0);
 
